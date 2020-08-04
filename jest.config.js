8cd4f5163bc7423cbf23dadff1768371745f81aa
Mahmood Sajjadi <mahmood@sondersafe.com>
2020-08-04 12:38:42 +1000

    add coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  "displayName": "TEST",
+  "displayName": "dius_tenis",
+  "collectCoverage": true,
   "roots": [
     "<rootDir>/src"
   ],
@@ -7,4 +8,31 @@ module.exports = {
   "transform": {
     "^.+\\.ts$": "ts-jest"
   },
+  "collectCoverageFrom": [
+    "**/*.ts"
+  ],
+  "coveragePathIgnorePatterns": [
+    'src/index.ts',
+    '/node_modules/',
+  ],
+  "coverageReporters": [
+    "lcov",
+    "text",
+    "text-summary",
+    "html"
+  ],
+  "coverageThreshold": {
+    "global": {  // global thresholds
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    },
+  },
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "json",
+    "node"
+  ]
 }

915d78c1e5dff1df113ee1775fb876554b02122b
Mahmood Sajjadi <mahmood@sondersafe.com>
2020-08-04 15:36:54 +1000

    [master] add missing config.ts file

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+export default {
+    setScoreToTieBrak: 6,
+    minScoreToWinSet: 6,
+    minDiffToWinSet: 2,
+    setScoreToWin: 7,
+    normal: {
+        matchPints: [0, 15, 30, 40],
+        miniPointsDeuce: 3,
+        minPointsToWinGame: 4,
+        minDiffToWinGame: 2
+    },
+    tieBrak: {
+        minPointsToWinGame: 7,
+        minDiffToWinGame: 2
+    }
+}
